4608936dadda42609416c062ff50899c93054685
DannySanchez03 <dannyproa303@gmail.com>
2024-05-21 06:33:04 -0500

    Almost done but Im happy with it

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Ensure this is imported for Bootstrap's JavaScript functionality
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function NavTabs() {
   const currentPage = useLocation().pathname;
@@ -9,20 +9,14 @@ function NavTabs() {
     <>
       <nav className="navbar navbar-expand-lg bg-opacity-0">
         <div className="container-fluid">
-          <div className="row w-100">
-            <div className="col">
-              <a className="navbar-brand" href="#">
-                <h1 className='display-1'>Danny</h1>
-              </a>
-            </div>
-            <div className="col d-flex justify-content-end">
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-            </div>
-          </div>
-          <div className="collapse navbar-collapse navUlPosition" id="navbarNavAltMarkup">
-            <div className="navbar-nav ms-auto"> {/* Add ms-auto for right alignment */}
+          <a className="navbar-brand" href="#">
+            <h1 className='display-1'>Danny</h1>
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse ML navbar-collapse navUlPosition" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
               <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>
                 <h1 className='display-6'>About Me</h1>
               </Link>
