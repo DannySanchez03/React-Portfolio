e1235c040c21998f5a8aef7f9094f7d74e6fe7cb
DannySanchez03 <dannyproa303@gmail.com>
2024-05-22 23:50:38 -0500

    Updated about me

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -7,25 +7,30 @@ export default function Footer() {
             <img src={pic} alt="A photo of me :)" className='img-fluid me-pic'/>
         </div>
         <p className="inside-text gradient-text">
-            Hey, I'm Danny, and welcome to my website. Let's see where to start 🤔, 
-            I am currently pursuing a Bachelor's Degree in Computer Science at The University of Texas Rio Grande Valley(UTRGV). 
-            I am currently 20 years old and a Senior, 
-            I have an older brother who graduated as a Mechanical Enginner and a older sister that graduated as a Marketing major. 
-            I was able to make this website because of the skills I learned from the UTSA FullStack Software Engineer bootcamp where I graduated with my certifcate for .this 😂. 
-            I am really trying to push myself to become a good Software Enginner so I can support the people closest to me. 
-            I am currently open and looking for internships either related to Computer Science with having to do with data or 
-            any type of positions for a Software Engineer (front-end or back-end)(is my preferred position).
-            I'd like to stay away from anything that requires a foundation of knowing AI, 
-            since at the moment I know nothing about it, 
-            but if the opportunity presents itself then I'd love to learn about it from any work or internship that comes my way.
-            At the moment I dont know many coding languages, I am just familiar with C++ and Java from which C++ was the language we had to use for our studies at UTRGV,
-            then I picked up Java and OOP for Java at UTRGV as a course, 
-            and now I am familiar with JavaScript along with learning HTML, CSS, and the Node.js runtime environment, being able to setup a MERN (Mongoose, Express, React, Node) stack application.
-            For now enjoy this speech of Lorem ipsum: 
-            ipsum dolor sit amet consectetur adipisicing elit. 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Hey there, I'm Danny, and it is my pleasure to welcome you to my website. 
+            Allow me to provide an introduction, currently cruising through a Bachelor's Degree in Computer science at The Texas Rio Grande Valley (UTRGV), 
+            I'm twenty years old and proudly stand as a senior, advancing steadily towards my academic goals. 
+            Within my family, my older brother conquered the mechanical engineering world and a sister who has flourished within the domain of marketing. 
+            Their successes serve as inspiration as I chart my own course in the realm of technology! 
+
+            Now, what's cooking on my plate? 💻Well, I've got my boots on the ground at a FullStack Software Engineer bootcamp with UTSA, 
+            a commitment I undertake with dedication. My aspiration is to cultivate an expertise in software engineering, 
+            driven by a desire to provide support to those closest to me. 
+
+            Currently, i'm on the hunt for internship that'll let me dive deep into the data-driven world of computer science or 
+            flex my muscles as a software engineer (front-end or back-end, you name it!).
+            🚀I'm steering clear of anything too AI-heavy for now, but hey, if the chance to dabble in the AI realm pops up, count me in for the adventure!
+
+
+            As for my coding toolkit, it's a work in progress. 
+            I've dabbled in C++ and Java during the UTRGV days, with Java and all its OOP goodness stealing the spotlight. 
+            Fast forward to now, I'm grooving with JavaScript, HTML, CSS, and strutting my stuff in the Node.js runtime environment. 
+            MERN stack? You betcha, I can whip up a storm with Mongoose, Express, React, and Node! 
+
+            So, who's ready to join me on this epic coding quest? No? Hear me out with this speech of Lorem ipsum: 
+            psum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
             Molestias eaque asperiores nemo quasi sint aliquam minus consequatur dolorum corporis consequuntur quas ducimus 
-            explicabo aliquid quisquam repudiandae dolorem odio, nostrum tempore!Lorem
+            explicabo aliquid quisquam repudiandae dolorem odio, nostrum tempore! Lorem.
         </p>
     </div>
     );
